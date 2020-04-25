871340927c83c4b9b03337a6f499c68d598e2bdb
Chris W <wells457@umn.edu>
2020-04-24 19:33:10 -0500

    added more comments

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// this makes it so when we deploy to heroku we can use our mLab databse, but for development we use our local database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
